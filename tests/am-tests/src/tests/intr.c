8bc40d65e5b47db4c9e03ee4a45b5740c0362df5
huxuan0307 <532391012@qq.com>
2021-09-25 16:52:05 +0800

    am-test: modify time interrupt handler

--- tests/am-tests/src/tests/intr.c
+++ tests/am-tests/src/tests/intr.c
@@ -1,8 +1,12 @@
 #include <amtest.h>
 
+#define CLINT_START       0x2000000
+#define CLINT_MTIMECMP    (CLINT_START + 0x4000)
+#define CLINT_MTIME       (CLINT_START + 0xBFF8)
 Context *simple_trap(Event ev, Context *ctx) {
   switch(ev.event) {
     case EVENT_IRQ_TIMER:
+      *((uint64_t*) CLINT_MTIMECMP) += 1000;
       putch('t'); break;
     case EVENT_IRQ_IODEV:
       putch('d'); break;
@@ -17,10 +21,13 @@ Context *simple_trap(Event ev, Context *ctx) {
 void hello_intr() {
   printf("Hello, AM World @ " __ISA__ "\n");
   printf("  t = timer, d = device, y = yield\n");
-  io_read(AM_INPUT_CONFIG);
-  iset(1);
-  for(int i=0; i<10; i++) {
+  io_read(AM_INPUT_CONFIG); // external interrupt
+  iset(1);  // interrupt enable
+  printf("mtimecmp: %d\n", *((uint64_t*) CLINT_MTIMECMP));
+  // *((uint64_t*) CLINT_MTIMECMP) += 5000;
+  // printf("mtimecmp: %d\n", *((uint64_t*) CLINT_MTIMECMP));
+  printf("mtime: %d\n", *((uint64_t*) CLINT_MTIME));
+  for(int i=0; i<100; i++) {
     yield();
-    printf("yield once\n");
   }
 }
